c4d10f5207f3ab19f40eecc0acd90d58a169f3df
Ayush Raj <mailtoayushh@gmail.com>
2024-10-31 16:19:47 +0530

    Updated hangman game with new features/changes

--- HangMang.py
+++ HangMang.py
@@ -6,6 +6,8 @@ word=random.choice(words)
 
 total_chance = 7
 
+total_point=100
+
 guessed_word = '_'*len(word)
 
 while (total_chance!=0):
@@ -17,11 +19,16 @@ while (total_chance!=0):
                 guessed_word=guessed_word[:index]+letter+guessed_word[index+1:]
 
         if guessed_word==word:
-            print("You guessed the word right")
+            print()
+            print("-------------You guessed the word right-------------")
+            print()
+            print("Total point is :",total_point)
+            print()
             break
 
     else:
         total_chance-=1
+        total_point=total_point-10
         print("You guessed the wrong letter and now total remaning chances are",total_chance)
 if (total_chance==0):
     print("Game Over as your all chances are exchausted...")
